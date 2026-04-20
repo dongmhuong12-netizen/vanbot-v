16de53dbb4a0c5047fa70f75e6f800df48d44c60
dongmhuong12-netizen <dongmhuong12@gmail.com>
2026-04-20 23:10:38 +0700

    Delete engine directory

--- engine/rule_engine.py
+++
@@ -1,9 +0,0 @@
-WARN_RULES = {
-    1: {"action": "warn"},
-    2: {"action": "mute", "duration": 600},
-    3: {"action": "kick"},
-    4: {"action": "ban"}
-}
-
-def get_action(level):
-    return WARN_RULES.get(level)
