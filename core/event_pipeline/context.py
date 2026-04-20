1299eaba1d5560f77c632858db9c6796cc4def2f
dongmhuong12-netizen <dongmhuong12@gmail.com>
2026-04-20 23:10:19 +0700

    Delete core directory

--- core/event_pipeline/context.py
+++
@@ -1,8 +0,0 @@
-class EventContext:
-    def __init__(self, message):
-        self.guild_id = message.guild.id if message.guild else None
-        self.user_id = message.author.id
-        self.channel_id = message.channel.id
-        self.message = message
-        self.content = message.content
-        self.author = message.author
--- core/event_pipeline/preprocessor.py
+++
@@ -1,9 +0,0 @@
-def preprocess(message):
-
-    return {
-        "guild_id": message.guild.id if message.guild else None,
-        "user_id": message.author.id,
-        "channel_id": message.channel.id,
-        "message": message,
-        "content": message.content
-    }
