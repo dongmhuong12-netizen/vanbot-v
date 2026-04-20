51d956d739c773b9489bb025478f26e36e79d1e3
dongmhuong12-netizen <dongmhuong12@gmail.com>
2026-04-20 22:09:01 +0700

    Delete bot/plugins/punishment/kick.py

--- bot/plugins/punishment/kick.py
+++
@@ -1,8 +0,0 @@
-# plugins/punishment/kick.py
-
-async def kick_user(member, reason=None):
-
-    try:
-        await member.kick(reason=reason or "No reason provided")
-    except Exception as e:
-        print("KICK ERROR:", repr(e))
