b467b103f142d913bb37dbd05e6c12aefba3b18c
dongmhuong12-netizen <dongmhuong12@gmail.com>
2026-04-20 23:10:46 +0700

    Delete plugins directory

--- plugins/punishment/kick.py
+++
@@ -1,5 +0,0 @@
-async def kick_user(member, reason=None):
-    try:
-        await member.kick(reason=reason or "No reason provided")
-    except Exception as e:
-        print("KICK ERROR:", repr(e))
--- plugins/punishment/mute.py
+++
@@ -1,9 +0,0 @@
-async def mute_user(event, duration=None):
-
-    member = event["message"].author
-
-    try:
-        await member.edit(timed_out_until=None)
-
-    except Exception as e:
-        print("MUTE ERROR:", repr(e))
